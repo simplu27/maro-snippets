8733dbe82d9564a91584c6fb02cd75883d1a4b61
simplu27 <simplu27@yahoo.com>
2016-11-30 16:34:03 +0200

    get user ip function

---
+++ php/get-user-ip.php
@@ -0,0 +1,26 @@
+<?php
+// http://stackoverflow.com/questions/13646690/how-to-get-real-ip-from-visitor
+
+function getUserIP() {
+
+    $client  = @$_SERVER['HTTP_CLIENT_IP'];
+    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+    $remote  = $_SERVER['REMOTE_ADDR'];
+
+    if(filter_var($client, FILTER_VALIDATE_IP)) {
+        $ip = $client;
+    } elseif(filter_var($forward, FILTER_VALIDATE_IP)) {
+        $ip = $forward;
+    } else {
+        $ip = $remote;
+    }
+
+    return $ip;
+}
+
+
+$user_ip = getUserIP();
+
+echo $user_ip; // Output IP address [Ex: 177.87.193.134]
+
+?>
